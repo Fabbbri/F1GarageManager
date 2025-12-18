d5398f6fc3fa6c7afc86ff07345517a40e96b3fa
Fabriii3G <fabgonzalez@estudiantec.cr>
2025-12-17 23:53:39 -0600

    Deleted useless archives

--- frontend/src/data/dummyUsers.js
+++
@@ -1,5 +0,0 @@
-export const dummyUsers = [
-  { id: "1", name: "Admin", email: "admin@test.com", password: "admin123", role: "ADMIN" },
-  { id: "2", name: "Engineer", email: "engineer@test.com", password: "eng123", role: "ENGINEER" },
-  { id: "3", name: "Driver", email: "driver@test.com", password: "drv123", role: "DRIVER" },
-];
